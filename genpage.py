30bb842dd711dc6d41a3f39bb3ac7f1a23c7ffbf
strv13570 <strv13570@gmail.com>
2018-08-13 01:05:26 +0900

    add UMEKUSA bit

--- genpage.py
+++ genpage.py
@@ -5,17 +5,35 @@ import math
 argv = sys.argv
 argc = len(argv)
 
-if argc < 2:
-    print("利用方法: python rebin.py [inputfile] [outputfile]")
-    quit()
+# if argc < 2:
+#     print("利用方法: python rebin.py [inputfile] [outputfile]")
+#     quit()
 
-pix = 1024
+argv.append("./kouka.bin")
+
+pix = 256
 binary = ''
 with open(argv[1], 'rb') as inf:
     binary = inf.readline()
 binary_str = binary.decode()
 
-print((pix*pix)/2)
-print(len(binary_str))
-page_num = math.ceil(len(binary_str) / int((pix*pix)/2))
-print(page_num)
+max_bits = int((pix*pix)/2)
+data_bits = len(binary_str)
+page_num = math.ceil(len(binary_str) / max_bits)
+
+over_bits = max_bits - data_bits
+over_bytes = int(over_bits / 8)
+
+print("ページサイズ:{0}x{0}".format(pix))
+print("使用ページ数:" + str(page_num))
+print("保存可能ビット:" + str(max_bits))
+print("保存データビット:" + str(data_bits))
+print("余剰ビット:" + str(over_bits))
+print("余剰バイト:" + str(over_bytes))
+
+for i in range(over_bytes):
+    if i % 2 == 0:
+        binary_str += "11101100"
+    elif i % 2 == 1:
+        binary_str += "00010001"
+
